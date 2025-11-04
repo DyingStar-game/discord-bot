5dfa907ab661d3f6e1474ed55eabd6f9e71a0f34
Dercraker <antoine.capitain@gmail.com>
2025-11-04 21:52:17 +0100

    push vite fait

--- src/commands/broadcast/index.ts
+++ src/commands/broadcast/index.ts
@@ -1,20 +1,49 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command } from '@sapphire/framework';
-import { send } from '@sapphire/plugin-editable-commands';
-import { ApplicationCommandType, ApplicationIntegrationType, InteractionContextType, type Message } from 'discord.js';
+import { Command, container } from '@sapphire/framework';
+import { Subcommand } from '@sapphire/plugin-subcommands';
+import { ApplicationIntegrationType, ChannelType, InteractionContextType } from 'discord.js';
 
-@ApplyOptions<Command.Options>({
+@ApplyOptions<Subcommand.Options>({
 	name: 'broadcast',
 	description: 'Broadcast a message to the server from bot account',
 	runIn: ['GUILD_TEXT'],
 	aliases: ['broadcast', 'br'],
-	typing: true
+	typing: true,
+	subcommands: [
+		{
+			name: 'send',
+			chatInputRun: async (interaction: Command.ChatInputCommandInteraction) => {
+				return await broadcast(interaction);
+			}
+		},
+		{
+			name: 'send2',
+			chatInputRun: async (interaction: Command.ChatInputCommandInteraction) => {
+				return await broadcast(interaction);
+			}
+		},
+		{
+			name: 'action',
+			type: 'group',
+			entries: [
+				{
+					name: 'senvd',
+					chatInputRun: async (interaction: Command.ChatInputCommandInteraction) => {
+						return await broadcast(interaction);
+					}
+				},
+				{
+					name: 'senvd2',
+					chatInputRun: async (interaction: Command.ChatInputCommandInteraction) => {
+						return await broadcast(interaction);
+					}
+				}
+			]
+		}
+	]
 })
-export class UserCommand extends Command {
-	// Register slash and context menu command
+export class BroadcastCommand extends Subcommand {
 	public override registerApplicationCommands(registry: Command.Registry) {
-		// Create shared integration types and contexts
-		// These allow the command to be used in guilds and DMs
 		const integrationTypes: ApplicationIntegrationType[] = [ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall];
 		const contexts: InteractionContextType[] = [
 			InteractionContextType.BotDM,
@@ -22,53 +51,66 @@ export class UserCommand extends Command {
 			InteractionContextType.PrivateChannel
 		];
 
-		// Register slash command
-		registry.registerChatInputCommand({
-			name: this.name,
-			description: this.description,
-			integrationTypes,
-			contexts
-		});
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.setIntegrationTypes(integrationTypes)
+				.setContexts(contexts)
+				.addSubcommand((subcommand) =>
+					subcommand
+						.setName('send')
+						.setDescription('Send a message to the server')
+						.addStringOption((option) => option.setName('message-id').setDescription('The message ID to send').setRequired(true))
+						.addChannelOption((option) =>
+							option
+								.setName('channel')
+								.setDescription('The channel to send the message to')
+								.setRequired(true)
+								.addChannelTypes([ChannelType.GuildText, ChannelType.GuildAnnouncement])
+						)
+				)
+				.addSubcommand((subcommand) =>
+					subcommand
+						.setName('send2')
+						.setDescription('Send a message to the server')
+						.addStringOption((option) => option.setName('message-id').setDescription('The message ID to send').setRequired(true))
+						.addChannelOption((option) =>
+							option
+								.setName('channel')
+								.setDescription('The channel to send the message to')
+								.setRequired(true)
+								.addChannelTypes([ChannelType.GuildText, ChannelType.GuildAnnouncement])
+						)
+				)
+				.addSubcommandGroup((group) =>
+					group
+						.setName('action')
+						.setDescription('Action to perform')
+						.addSubcommand((subcommand) => subcommand.setName('senvd').setDescription('Send a message to the server'))
+						.addSubcommand((subcommand) => subcommand.setName('senvd2').setDescription('Send a message to the server'))
+				)
+		);
 
-		// Register context menu command available from any message
-		registry.registerContextMenuCommand({
-			name: this.name,
-			type: ApplicationCommandType.Message,
-			integrationTypes,
-			contexts
-		});
+		// registry.registerContextMenuCommand({
+		// 	name: this.name,
+		// 	type: ApplicationCommandType.Message,
+		// 	integrationTypes,
+		// 	contexts
+		// });
 	}
 
-	// Message command
-	public override async messageRun(message: Message) {
-		const msg = await send(message, 'Ping?');
-
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)
-		}ms.`;
-
-		return send(message, content);
-	}
-
-	// slash command
-	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const msg = await interaction.reply({ content: 'Ping?', fetchReply: true });
-
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			msg.createdTimestamp - interaction.createdTimestamp
-		}ms.`;
-
-		return interaction.editReply({ content });
-	}
-
-	// context menu command
-	public override async contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
-		const msg = await interaction.reply({ content: 'Ping?', fetchReply: true });
-
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			msg.createdTimestamp - interaction.createdTimestamp
-		}ms.`;
-
-		return interaction.editReply({ content });
-	}
+	// public override async contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
+	// 	return await broadcast(interaction);
+	// }
 }
+
+const broadcast = async (interaction: Command.ContextMenuCommandInteraction | Command.ChatInputCommandInteraction) => {
+	const msg = await interaction.reply({ content: 'Ping?', fetchReply: true });
+
+	const content = `Broadcasted! Bot Latency ${Math.round(container.client.ws.ping)}ms. API Latency ${
+		msg.createdTimestamp - interaction.createdTimestamp
+	}ms.`;
+
+	return interaction.editReply({ content });
+};
